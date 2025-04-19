54df5ab3a0d385e12d3705cfc791c906e8629b65
jheel-phantom <jhljheel@gmail.com>
2025-04-19 21:36:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to Omnia Inc.</h1>
+      <p>We are a leading technology company driving innovation.</p>
+      <p>This site is currently under construction.</p>
+    </>
+  )
+}
+
+export default App
